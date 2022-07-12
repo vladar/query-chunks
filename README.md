04d9a582b7f859722f38fdf8ec6f7119995188c3
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2022-07-12 11:36:51 +0200

    README + cleanup a bit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Tree-shakeable query resolvers
+
+Proof of concept for tree-shakeable GraphQL resolvers with webpack.
+
+Answers the question: is it possible to define resolvers for GraphQL type in a tree-shakeable way
+without falling back to hacks and custom plugins.
+
+See `resolvers-package1/index.js` or `resolvers-package2/index.js` for required file structure.
--- webpack.config.js
+++ webpack.config.js
@@ -3,21 +3,10 @@ module.exports = {
   optimization: {
     minimize: true,
     splitChunks: {
-      /* Allows optimizations for both initial and async chunks. Defaults to 'async'. */
-      /* Note: if your experience shows blank page with no errors in console, override this value
-         in your experience's webpack config file to 'async' */
       chunks: "all",
-      /* Chunk splitting options. */
+      // enforce chunking:
       minSize: 1,
-      // minChunks: 1,
-      // maxAsyncRequests: 1,
-      // maxInitialRequests: 1,
-      cacheGroups: {
-        // resolvers: {
-        //   test: /resolvers/,
-        //   chunks: "all",
-        // },
-      },
+      minChunks: 2,
     },
   },
 };
